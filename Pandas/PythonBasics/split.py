39f7f1e421f18a8956b1854a972dc73a669cfbf1
Rohyth <Rohitratawal@gmail.com>
2020-05-02 21:54:19 +0530

    try_django

---
+++ Pandas/PythonBasics/split.py
@@ -0,0 +1,13 @@
+Alpha = 'Am a great Software developer. Will beat All with time.'
+Alpha
+
+l = Alpha.split(' ')
+
+for i in range(len(l)):
+    print(l[i] + chr(10))
+    
+
+#join example
+
+Joined = ' and '.join(l) 
+Joined
