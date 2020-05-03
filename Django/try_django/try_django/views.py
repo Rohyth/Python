b0e3fda81ca4ab673bff8e687b91d9746ba98c55
Rohyth <Rohitratawal@gmail.com>
2020-05-03 07:37:45 +0530

    dj_cmt

--- Django/try_django/try_django/views.py
+++ Django/try_django/try_django/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request,"Home.html")
+    title = "Home"
+    return render(request,"Home.html",{'title':title})
     
 
 def about_page(request):
+    title = "About"
     return HttpResponse("<h1> Rohyth is Awesome </h1>")
 
 def contact_page(request):
+    title = "Contact"
     return HttpResponse("<h1> Rohyth is Smart </h1>")
