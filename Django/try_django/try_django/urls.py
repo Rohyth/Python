4e6ee3893aa376982fd0ee15b19b8d0dab42539e
Rohyth <Rohitratawal@gmail.com>
2020-05-04 12:30:36 +0530

    try_dj

--- Django/try_django/try_django/urls.py
+++ Django/try_django/try_django/urls.py
@@ -1,6 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from blog.views import (
+    blog_post_details_page,
+)
+
 from .views import (
     home_page,
     about_page,
@@ -11,5 +15,7 @@ urlpatterns = [
     path('',home_page),
     path('about/',about_page),
     path('contact/',contact_page),
+    path('details/<str:slug>/',blog_post_details_page),
     path('admin/', admin.site.urls),
+
 ]   
