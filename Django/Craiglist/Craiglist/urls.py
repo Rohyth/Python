3eb50222bfbe22f62a345a7496d26f8e2751ff76
Rohyth <Rohitratawal@gmail.com>
2020-05-25 03:14:23 +0530

    Craiglist

--- Django/Craiglist/Craiglist/urls.py
+++ Django/Craiglist/Craiglist/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from MyApp.views import home
+from django.urls import path,include
 
 urlpatterns = [
-    path('',home),
+    path('',include('MyApp.urls')),
     path('admin/', admin.site.urls),
+
 ]
