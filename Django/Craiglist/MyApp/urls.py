8fbab2906eb965079d9ab633e3a3ae1f10086a8b
Rohyth <Rohitratawal@gmail.com>
2020-05-25 04:51:48 +0530

    DakkuDaddy's List

--- Django/Craiglist/MyApp/urls.py
+++ Django/Craiglist/MyApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,new_search
+
 
 urlpatterns = [
     path('',home),
-
+    path('new_search/',new_search),
 ]
--- Django/Craiglist/MyApp/views.py
+++ Django/Craiglist/MyApp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 
 def home(request):
+    return render(request,'Base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
     return render(request,'index.html')
