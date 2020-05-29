0ae87629a36dcef26e6f488dad5645b3f5676627
Rohyth <Rohitratawal@gmail.com>
2020-05-29 22:53:22 +0530

    class_n_funcs

---
+++ Pandas/PythonBasics/Class_functions.py
@@ -0,0 +1,44 @@
+'Class n functions'
+
+'declare a function using keyword - "def"'
+
+'Empty Apartment --> Kitchen/Bedroom/Bathroom (functions)'
+
+def dcsc(inc,vari):
+    z = inc*vari
+    print('deduction coding by dcsc :',z)
+
+def ssa(inc):
+    inc = inc*3
+    print(inc)
+    
+def collection():
+    print('recovery')
+
+
+
+class Kraft():
+    inc = 5000
+    vari = 20
+    
+    #dcsc(inc,vari)
+    ssa(inc)
+    
+    
+#Using return with funcs
+def Calc(xyz):
+    xyz = xyz**2
+    abc = xyz+2
+    return xyz,abc,'Hello Nupur Ji'
+
+Calc(3)
+
+#Using Lambda Functions-----------------------
+
+Mult = lambda a,b : a*b
+Mult(2, 5)
+
+
+
+
+
