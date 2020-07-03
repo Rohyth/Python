4f68b222f73c5a6ae3eb32091247488200ba2585
Rohyth <Rohitratawal@gmail.com>
2020-07-04 05:24:22 +0530

    Blogger_2

--- Django/Blogger/Blog/views.py
+++ Django/Blogger/Blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'Blog/Home.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'Blog/About.html', context)
